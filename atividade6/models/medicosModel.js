219cfee4c98bedd27a6f804c15047547b8bf46cb
mascedo <americomascedo@gmail.com>
2025-03-14 15:01:54 -0300

    atividade 6

--- atividade6/models/medicosModel.js
+++ atividade6/models/medicosModel.js
@@ -1,3 +1,3 @@
-let medicos = [{id: 131,nome: 'romeu', especialidade: 'psicologia'},{id: 1331818991922,nome: 'juan', especialidade: 'cardiologia'},{id: 1155818991922,nome: 'leticia', especialidade: 'neurologia'}];
+let medicos = [];
 
 module.exports = medicos;
--- atividade6/models/pacientesModel.js
+++ atividade6/models/pacientesModel.js
@@ -1,3 +1,3 @@
-let pacientes = [{id: 13,nome: 'pablo', dataNascimento: '09/03/1962'},{id: 1311810991922,nome: 'fernando', dataNascimento: '21/09/1990'},{id: 1198218991922,nome: 'alonso', dataNascimento: '22/01/2001'}];
+let pacientes = [];
 
 module.exports = pacientes;
